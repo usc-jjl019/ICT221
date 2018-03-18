f775480fb61c1c749d26237b2b6322b1026baf63
usc-jjl019 <jjl019@student.usc.edu.au>
2018-03-18 13:58:50 +1000

    Code not working, but making progress on week 2 formatting

--- src/au/edu/usc/mathgame/Main.java
+++ src/au/edu/usc/mathgame/Main.java
@@ -10,6 +10,75 @@ import java.util.Scanner;
  * @author ogboo3
  */
 
+/** Class  Main, it's method is gameNumberQuestion used to prompt for how many games the user desires **/
+
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.gameNumberQuestion();
+    }
+
+    /**
+     * This is a for loop that runs depending on the input number the user has chosen
+     **/
+
+    public void gameNumberQuestion() {
+        System.out.println("10 questions per game, how many rounds would you like to play? ");
+        Scanner input = new Scanner(System.in);
+        int rounds = input.nextInt();
+        int roundsCount;
+        roundsCount = rounds * 10;
+        for (int i = 0; i < rounds; ++i) {
+            System.out.println("You have elected to play" + " " + roundsCount + " " + "rounds");
+            game();
+
+        }
+    }
+
+    int rounds;
+
+    /**
+     * This starts the game using the parameters set from gameNumberQuestion
+     **/
+
+    public void game() {
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 10; ++i) {
+            Question question1 = new Question();
+            question1.showQuestion();
+            int response = input.netInt();
+            if (response == question1.answer) {
+                ++counter;
+                System.out.println(count + " " + "is your score" + (++i));
+            }
+        }
+
+    }
+}
+
+public class Question {
+    int answer;
+
+    Random rand = new Random();
+}
+
+
+
+
+
+
+
+
+
+
+/**
+}
+
+
+
+
 public class Question {
     private int value1;
     private int value2;
@@ -59,8 +128,9 @@ public class Main {
             } else {
                 System.out.printf("  No, the answer is %d.\n", correct);
             }
-        } **/
+        }
     }
 }
 
 
+**/
